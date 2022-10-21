52bcc43181f10668d851715b44e8ec89e12259fe
leoazevedo0622 <lmsa@cesar.school>
2022-10-20 23:00:49 -0300

    Added a HttpRequest

--- weForum/urls.py
+++ weForum/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def myView(request):
+    return HttpResponse("Welcome to the main page!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', myView),
 ]
