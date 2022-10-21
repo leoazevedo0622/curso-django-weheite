be9a3c0e39310a98904678f1db2b045323655593
leoazevedo0622 <lmsa@cesar.school>
2022-10-20 23:51:03 -0300

    Added templates

--- GameForum/views.py
+++ GameForum/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the Home page!")
+    return render(request, 'GameForum/home.html')
